ed32bc94958f181d1d213228e32958d3dfe334e3
pierluigicarrieri <pierluigi.carrieri.90@gmail.com>
2023-08-21 18:36:40 +0200

    Exercise done

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,17 @@
 <script>
-  export default {
 
-  }
+  import Title from "./components/AppTitle.vue"
+
+  export default {
+    components: {
+      Title
+    }
+}
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <Title></Title>
 </template>
 
 <style>
